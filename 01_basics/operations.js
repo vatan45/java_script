d10225f41a3406a33d40a0b0eb0847c132bef325
vatan malik <142028575+vatan45@users.noreply.github.com>
2023-10-07 14:20:56 +0000

    all types of important operations are covered like add sub incr decr

---
+++ 01_basics/operations.js
@@ -0,0 +1,36 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Vatan"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2 );
+console.log(1 + "2");
+console.log("1" + 2 +2);
+console.log(1 + 2 + "2");
+
+console.log(+true); // output will be 1
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+gamecounter++;
+
+console.log(gamecounter);
+++gamecounter;
+console.log(gamecounter);
+
+
+ 
