167c509aea167b0705a97dc0df59bb10511eee07
vatan malik <142028575+vatan45@users.noreply.github.com>
2023-10-07 15:36:15 +0000

    primitive and non primitive datatypes all covered

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,34 @@
+//primitive
+
+// 7 types : String , number, boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scorevalue = 100
+
+const isLoggedIn = false
+const outsidetemp = null
+let useremail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id == anotherid);
+
+const bignumber = 3534567543456543n
+
+const heros = ["shaktiman, naagraj, doga"]
+let myObj = {
+    name: "Vatan",
+    age: 22
+    
+}
+
+const MyFunction = function(){
+    console.log("hello world");
+}
+
+console.log(typeof bignumber);
+
+
+//reference type(non-primitive)
+
+// Arrays, Objects, Functions
