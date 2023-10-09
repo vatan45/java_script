e4c46d96ff409aaac9d917f28d4c05f4fa1826eb
vatan malik <142028575+vatan45@users.noreply.github.com>
2023-10-09 10:42:02 +0000

    learned about strings and the operations on it

---
+++ 01_basics/strings.js
@@ -0,0 +1,39 @@
+const name = "vatan"
+const repocount = 50
+
+console.log(name + repocount + "value"); //outdated
+
+console.log('hello my name is ${name} and my repo cunt is ${repocount}' );
+
+
+const gameName = new String('vatan-vm')
+
+console.log(gameName[0]);
+console.log(gameName. __proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+ const newString = gameName.substring(0, 4)
+ console.log(newString);
+
+ const anotherstring = gameName.slice(-7, 2)
+ console.log(anotherstring);
+
+ const newstringone = "     vatan     "
+ console.log(newstringone);
+ console.log(newstringone.trim());
+
+
+ cosnt = url = "htps://vatan.com/vatan%20malik"
+ console.log(url.replace('%20', '-'));
+
+ console.log(url.includes('vatan'));
+
+ console.log(gameName.split('-'));
+
+ 
